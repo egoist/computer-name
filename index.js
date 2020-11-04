180ac9dfddd0833a606d63df17753d3685939bee
hopejr <hopejr@gmail.com>
2020-11-04 23:28:50 +1100

    Update index.js
    Added ability to return pretty name on Linux, or just hostname if the pretty name isn't set.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ module.exports = function () {
       return process.env.COMPUTERNAME
     case 'darwin':
       return cp.execSync('scutil --get ComputerName').toString().trim()
+    case 'linux':
+      const prettyname = cp.execSync('hostnamectl --pretty').toString().trim()
+      return prettyname === '' ? os.hostname() : prettyname
     default:
       return os.hostname()
   }
